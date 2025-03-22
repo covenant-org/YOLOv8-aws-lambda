dcacde5aeab3d984a519576a3584c4d0e38ef1ea
My name <me@example.com>
2025-03-21 23:37:07 -0600

    added needed libraries to requirements.txt, added code to handle callback from s3 put object trigger, added logging information

--- lambda-codebase/app.py
+++ lambda-codebase/app.py
@@ -4,36 +4,56 @@ import base64
 from io import BytesIO
 from PIL import Image
 import logging
+import boto3
+import urllib
+
+# Initialize S3 client
+s3 = boto3.client('s3')
 
 # Initialize YOLOv8 object detector
 yolov8_detector = YOLOv8('./models/yolov8s.onnx')
 
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel("INFO")
 
 def main(event, context):
 
-    logger.debug("Event received: %s", event)
+    logger.info("Event received: %s", event)
+    print("Event received: ", event)
 
-    # get payload
-    body = json.loads(event['body']) if isinstance(event['body'], str) else event['body']
+    if 'body' in event:
+        # get payload
+        try: 
+            body = json.loads(event['body']) if isinstance(event['body'], str) else event['body']
 
-    # get params
-    img_b64 = body['image']
-    size = body.get('size', 640)
-    conf_thres = body.get('conf_thres', 0.3)
-    iou_thres = body.get('iou_thres', 0.5)
+            # get params
+            img_b64 = body['image']
+            size = body.get('size', 640)
+            conf_thres = body.get('conf_thres', 0.3)
+            iou_thres = body.get('iou_thres', 0.5)
 
-    # open image
-    img = Image.open(BytesIO(base64.b64decode(img_b64.encode('ascii'))))
+            # open image
+            img = Image.open(BytesIO(base64.b64decode(img_b64.encode('ascii'))))
 
-    # infer result
-    detections = yolov8_detector(img, size=size, conf_thres=conf_thres, iou_thres=iou_thres)
+            # infer result
+            detections = yolov8_detector(img, size=size, conf_thres=conf_thres, iou_thres=iou_thres)
 
-    # return result
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "detections": detections
-        }),
-    }
+            # return result
+            return {
+                "statusCode": 200,
+                "body": json.dumps({
+                    "detections": detections
+                }),
+            }
+        except Exception as e:
+            logger.error("Error: %s", e)
+    else:
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+        
+        try: 
+            response = s3.get_object(Bucket=bucket, Key=key)
+            logger.info("S3 response: %s", response)
+        except Exception as e:
+            logger.error("Error: %s", e)
+            raise e
